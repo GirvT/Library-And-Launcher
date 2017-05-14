c2467721e838565085c9616e374ce306e88d296b
KeithCh <keithchow101@yahoo.com>
2017-05-14 00:23:32 -0400

    Improved game
    added a second moving object,
    added restart button,
    finish line detection still somewhat odd

--- Games/second.java
+++ Games/second.java
@@ -7,7 +7,8 @@ import java.awt.geom.*;
    
       Timer t = new Timer(30,this);
       double x = 0, y = 0;
-      int a = 100, b = 100;
+      int a = 100, b = 50;
+      int c = -250, d = 100;
       
       public second(){
          t.start();
@@ -20,28 +21,35 @@ import java.awt.geom.*;
          super.paintComponent(g);
          g.setFont(new Font("Helvetica", Font.BOLD, 24));
          g.setColor(Color.red);  
-         g.drawRect(540,340,50,50);
+         g.drawRect(50,500,50,50);
          g.setColor(Color.green);
          g.drawRect(0,150,500,300);
          g.setColor(Color.blue);
          g.drawRect(a,b,50,50);
+         g.drawRect(c,d,50,50);
          g.drawString("Move the black square to the red square.", 175,100);
          g.drawString("Don't try crossing me >:(", 150,250);
          Graphics2D g2 = (Graphics2D) g;
          g.setColor(Color.black);
          g2.fill(new Rectangle2D.Double(x, y, 50, 50));
-         if (x == 540 && y == 340){
+         if (x == 50 && y == 500) 
+            {
    	      g.setColor(Color.WHITE);
    	      g.fillRect(0, 0, getWidth(), getHeight());
    	      g.setColor(Color.BLACK);
    	      g.drawString("You Win!", 240,100);
    	      g.drawString("Press R to play again", 180,150);
-         }else if (x >a-50 && y>b-50 && y<b+50 && x<a+50){
+         
+         
+            }
+         else if (x >a-50 && y>b-50 && y<b+50 && x<a+50||x>c-50 && y>d-50 && y<d+50 && x<c+50)
+            {
    	      g.setColor(Color.WHITE);
    	      g.fillRect(0, 0, getWidth(), getHeight());
    	      g.setColor(Color.BLACK);
    	      g.drawString("You Lose! Press R to restart", 140,100);
-         }else if (x < 501 && y+50 > 150 ){
+            }
+         else if (x < 500 && y+50 > 150 && y+50<500) {
             g.setColor(Color.WHITE);
             g.fillRect(0, 0, getWidth(), getHeight());
             g.setColor(Color.BLACK);
@@ -55,25 +63,42 @@ import java.awt.geom.*;
             a = 0;
          } else {
             repaint();
-            a += 8;
+            a += 10;
          }
-      }
+         if (c>=800)
+            {
+            repaint();
+            c = -100;
+            }
+         else
+            {
+            repaint();
+            c +=6;   
+            }
+         if (y>=250)
+            {
+            b = 450;
+            d = 510;
+            repaint();  
+            }
+      }        
       
       public void up(){
-         y -= 5;
+         y -= 10;
       }
       
       public void down(){
-         y += 5;
+         y += 10;
       }
       
       public void left(){
-         x -= 5;
+         x -=9;
       }
       
       public void right(){
-         x += 5;
+         x += 7;
       }
+      
       
       public void keyPressed(KeyEvent e){
          int code = e.getKeyCode();
@@ -84,7 +109,16 @@ import java.awt.geom.*;
          if (code == KeyEvent.VK_LEFT)
             left();
          if (code == KeyEvent.VK_RIGHT)
-            right();    
+            right();  
+         if (code == KeyEvent.VK_R)
+            {
+            x = 0;
+            y = 0;
+            a = 100;
+            b = 50;
+            c = -250;
+            d = 100;
+            }
       }
       
       public void keyTyped(KeyEvent e){}
